43751d737d8a419228a1a033b847cccb099373d7
Hilmi12309 <hilmimax109@gmail.com>
2025-05-25 22:02:33 +0700

    Update off.js

--- js/off.js
+++ js/off.js
@@ -1,1 +1,22 @@
+// Daftarkan Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
+
+// Listen for messages from Service Worker
+navigator.serviceWorker.addEventListener('message', event => {
+  if (event.data === 'redirect-to-index' && navigator.onLine) {
+    window.location.href = 'index.html';
+  }
+});
+
+// Cek status koneksi secara periodik
+setInterval(() => {
+  if (navigator.onLine) {
+    window.location.href = 'index.html';
+  }
+}, 5000); // Cek setiap 5 detik
+
 
